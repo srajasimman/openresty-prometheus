37c08dd04b3ecf4f02ba23b830a39d5832060694
Rajasimman S <srajasimman@gmail.com>
2023-07-01 10:56:12 +0530

    :wrench: small changes

---
+++ docker_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]
+  then
+    tag='latest'
+  else
+    tag=$1
+fi
+
+docker rmi srajasimman/$(basename $(pwd)):$tag ;\
+docker build -t srajasimman/$(basename $(pwd)):$tag . && \
+docker push srajasimman/$(basename $(pwd)):$tag
