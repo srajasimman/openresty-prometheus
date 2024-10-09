5482478682d3967373297d758f6ec107b6da321d
Rajasimman S <srajasimman@gmail.com>
2024-10-09 15:54:47 +0000

    updated

--- docker_build.sh
+++ docker_build.sh
@@ -7,5 +7,6 @@ if [ $# -eq 0 ]
 fi
 
 docker rmi srajasimman/$(basename $(pwd)):$tag ;\
-docker build -t srajasimman/$(basename $(pwd)):$tag . && \
+docker build -t srajasimman/$(basename $(pwd)):$tag -t ghcr.io/srajasimman/$(basename $(pwd)):$tag . && \
 docker push srajasimman/$(basename $(pwd)):$tag
+docker push ghcr.io/srajasimman/$(basename $(pwd)):$tag
